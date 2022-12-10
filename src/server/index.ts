0ea07c4d75cb83e387236f5268b2a7e48f30901b
israel mailman <80362556+likefools@users.noreply.github.com>
2022-12-11 00:10:59 +0200

    test

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import { remultExpress } from 'remult/remult-express';
 import { api } from './api';
+import { JsonDataProvider } from 'remult';
+import { JsonEntityFileStorage } from 'remult/server';
 
 const app = express();
 app.use(api);
 
-
-app.listen(3002, () => console.log("Server started"));
+app.use(remultExpress({
+    dataProvider: async () => new JsonDataProvider(new JsonEntityFileStorage('./db'))
+}));
+// app.listen(3002, () => console.log("Server started"));
